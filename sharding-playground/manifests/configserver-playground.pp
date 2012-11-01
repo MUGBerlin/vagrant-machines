28024f3aa3b0abc79445ca2629e2400ad8b8bba7
Stefan Rudnitzki <stefanrudnitzki@googlemail.com>
2012-11-01 13:23:04 +0100

    fixed puppet file, typo in configserver name

--- sharding-playground/manifests/configserver-playground.pp
+++ sharding-playground/manifests/configserver-playground.pp
@@ -28,13 +28,13 @@ file { "/tmp/mongo_configdb":
 }
 
 exec { "start-cfg":
-	command => "/usr/bin/mongod --configsvr --dbpath /tmp/mongo_configdb/ --port 27018",
+	command => "/usr/bin/mongod --configsvr --dbpath /tmp/mongo_configdb/ --port 27018 &",
     	path    => "/usr/local/bin/:/bin/",
 	require => [ File['/tmp/mongo_configdb'], Package['mongodb']]
 }
 
 exec { "start-mongos":
-        command => "/usr/bin/mongos --configdb shard04.local:27018 --port 27019",
+        command => "/usr/bin/mongos --configdb shard04.local:27018 --port 27019 &",
         path    => "/usr/local/bin/:/bin/",
         require => [ Exec['start-cfg'], Package['mongodb']]
 }
