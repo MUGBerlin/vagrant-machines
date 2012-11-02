f216b165fb82e960831f4d0ace39bdd321fd371a
Stefan Rudnitzki <stefanrudnitzki@googlemail.com>
2012-11-02 10:52:43 +0100

    switched mongodb version to 10gen deb package
    needed a fresher version, because 2.0.4 has a sharding bug

--- sharding-playground/manifests/configserver-playground.pp
+++ sharding-playground/manifests/configserver-playground.pp
@@ -4,37 +4,49 @@ group { "puppet":
         ensure => "present",
 }
 
-notify{"Installing Packages!":}
+notify { "Installing Packages!": }
 
-package{ "mongodb":
-        ensure => present,
-        require => Notify['Installing Packages!'],
+file {"/etc/apt/sources.list.d/10gen.list":
+  ensure  => "present",
+  source  => "/vagrant/manifests/10gen.list",
+  require => Notify['Installing Packages!'],
 }
 
-file{"/etc/mongodb.conf":
-  ensure => "present",
-  source => "/vagrant/manifests/mongodb.conf",
-  require => Package['mongodb'],
+exec { "add-10genkey":
+  command => "/usr/bin/apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10 && /usr/bin/apt-get update",
+  path    => "/usr/local/bin/:/bin/:/usr/bin/",
+  require => File['/etc/apt/sources.list.d/10gen.list'],
 }
 
-package{ "libnss-mdns":
+package { "mongodb-10gen":
         ensure => present,
-        require => Notify['Installing Packages!']
+        require => Exec['add-10genkey'],
+}
+
+file { "/etc/mongodb.conf":
+  ensure  => "present",
+  source  => "/vagrant/manifests/mongodb.conf",
+  require => Package['mongodb-10gen'],
+}
+
+package { "libnss-mdns":
+  ensure  => present,
+  require => Notify['Installing Packages!']
 }
 
 file { "/tmp/mongo_configdb":
-	ensure => directory,
+	ensure  => directory,
 	require => Notify['Installing Packages!']
 }
 
 exec { "start-cfg":
-	command => "/usr/bin/mongod --configsvr --dbpath /tmp/mongo_configdb/ --port 27018 &",
-    	path    => "/usr/local/bin/:/bin/",
-	require => [ File['/tmp/mongo_configdb'], Package['mongodb']]
+	command => "/usr/bin/mongod --configsvr --dbpath /tmp/mongo_configdb/ --port 27018 > /tmp/mongocfg.log &",
+  path    => "/usr/local/bin/:/bin/",
+	require => [ File['/tmp/mongo_configdb'], Package['mongodb-10gen']]
 }
 
 exec { "start-mongos":
-        command => "/usr/bin/mongos --configdb configsrv.local:27018 --port 27019 &",
-        path    => "/usr/local/bin/:/bin/",
-        require => [ Exec['start-cfg'], Package['mongodb']]
+  command => "/bin/sleep 10 && /usr/bin/mongos --configdb configsrv.local:27018 --port 27019 > /tmp/mongos.log &",
+  path    => "/usr/local/bin/:/bin/",
+  require => [ Exec['start-cfg'], Package['mongodb-10gen']]
 }
--- sharding-playground/manifests/sharding-playground.pp
+++ sharding-playground/manifests/sharding-playground.pp
@@ -1,29 +1,40 @@
 # -*- mode: ruby -*-
 
 group { "puppet":
-        ensure => "present",
-}
-
-notify{"Installing Packages!":}
-
-package{ "mongodb":
-        ensure => present,
-        require => Notify['Installing Packages!'],
-}
-
-file{"/etc/mongodb.conf":
   ensure => "present",
-  source => "/vagrant/manifests/mongodb.conf",
-  require => Package['mongodb'],
+}
+
+notify { "Installing Packages!": }
+
+file {"/etc/apt/sources.list.d/10gen.list":
+  ensure  => "present",
+  source  => "/vagrant/manifests/10gen.list",
+  require => Notify['Installing Packages!'],
+}
+
+exec { "add-10genkey":
+  command => "/usr/bin/apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10 && /usr/bin/apt-get update",
+  path    => "/usr/local/bin/:/bin/:/usr/bin/",
+  require => File['/etc/apt/sources.list.d/10gen.list'],
+}
+
+package { "mongodb-10gen":
+        ensure => present,
+        require => Exec['add-10genkey'],
+}
+
+file { "/etc/mongodb.conf":
+  ensure  => "present",
+  source  => "/vagrant/manifests/mongodb.conf",
+  require => Package['mongodb-10gen'],
+}
+
+package { "libnss-mdns":
+  ensure  => present,
+  require => Notify['Installing Packages!']
 }
 
 service{"mongodb":
         ensure => running,
         subscribe => File['/etc/mongodb.conf'],
 }
-
-package{ "libnss-mdns":
-        ensure => present,
-        require => Notify['Installing Packages!']
-}
-
