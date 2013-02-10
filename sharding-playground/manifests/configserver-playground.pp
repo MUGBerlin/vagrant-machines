20c53cc000078e357a68ca360266446973a3fefd
strud <stefanrudnitzki@googlemail.com>
2013-02-10 19:34:53 +0100

    formatting

--- sharding-playground/manifests/configserver-playground.pp
+++ sharding-playground/manifests/configserver-playground.pp
@@ -17,35 +17,35 @@ file { '/tmp/mongo_configdb':
 }
 
 file { '/etc/apt/sources.list.d/10gen.list':
-  	ensure  => 'present',
-  	source  => '/vagrant/manifests/10gen.list',
+  ensure  => 'present',
+  source  => '/vagrant/manifests/10gen.list',
 }
 
 exec { 'add-10genkey':
-  	command => '/usr/bin/apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10 && /usr/bin/apt-get update',
-  	path    => '/usr/local/bin/:/bin/:/usr/bin/',
-  	require => File['/etc/apt/sources.list.d/10gen.list'],
+  command => '/usr/bin/apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10 && /usr/bin/apt-get update',
+  path    => '/usr/local/bin/:/bin/:/usr/bin/',
+  require => File['/etc/apt/sources.list.d/10gen.list'],
 }
 
 package { 'mongodb-10gen':
-  	ensure  => present,
-  	require => Exec['add-10genkey'],
+  ensure  => present,
+  require => Exec['add-10genkey'],
 }
 
 file { '/etc/mongodb.conf':
-  	ensure  => present,
-  	source  => '/vagrant/manifests/mongodb.conf',
-  	require => Package['mongodb-10gen'],
+  ensure  => present,
+  source  => '/vagrant/manifests/mongodb.conf',
+  require => Package['mongodb-10gen'],
 }
 
 exec { 'start-cfg':
 	command => '/usr/bin/mongod --configsvr --dbpath /tmp/mongo_configdb/ --port 27018 > /tmp/mongocfg.log &',
-  	path    => '/usr/local/bin/:/bin/',
+  path    => '/usr/local/bin/:/bin/',
 	require => [ File['/tmp/mongo_configdb'], Package['mongodb-10gen'] ],
 }
 
 exec { 'start-mongos':
-  	command => '/bin/sleep 30 && /usr/bin/mongos --configdb configsrv.local:27018 --port 27019 > /tmp/mongos.log &',
-  	path    => '/usr/local/bin/:/bin/',
-  	require => [ Exec['start-cfg'], Package['mongodb-10gen'] ],
+  command => '/bin/sleep 60 && /usr/bin/mongos --configdb configsrv.local:27018 --port 27019 > /tmp/mongos.log &',
+  path    => '/usr/local/bin/:/bin/',
+  require => [ Exec['start-cfg'], Package['mongodb-10gen'] ],
 }
--- sharding-playground/manifests/sharding-playground.pp
+++ sharding-playground/manifests/sharding-playground.pp
@@ -12,33 +12,29 @@ package { 'libnss-mdns':
 	ensure  => present,
 }
 
-file { '/tmp/mongo_configdb':
-	ensure  => directory,
-}
-
 file { '/etc/apt/sources.list.d/10gen.list':
-  	ensure  => 'present',
-  	source  => '/vagrant/manifests/10gen.list',
+	ensure  => 'present',
+	source  => '/vagrant/manifests/10gen.list',
 }
 
 exec { 'add-10genkey':
-  	command => '/usr/bin/apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10 && /usr/bin/apt-get update',
-  	path    => '/usr/local/bin/:/bin/:/usr/bin/',
-  	require => File['/etc/apt/sources.list.d/10gen.list'],
+  command => '/usr/bin/apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10 && /usr/bin/apt-get update',
+  path    => '/usr/local/bin/:/bin/:/usr/bin/',
+	require => File['/etc/apt/sources.list.d/10gen.list'],
 }
 
 package { 'mongodb-10gen':
-  	ensure  => present,
-  	require => Exec['add-10genkey'],
+	ensure  => present,
+	require => Exec['add-10genkey'],
 }
 
 file { '/etc/mongodb.conf':
-  	ensure  => present,
-  	source  => '/vagrant/manifests/mongodb.conf',
-  	require => Package['mongodb-10gen'],
+	ensure  => present,
+	source  => '/vagrant/manifests/mongodb.conf',
+	require => Package['mongodb-10gen'],
 }
 
 service{ 'mongodb':
-    ensure => running,
-    subscribe => File['/etc/mongodb.conf'],
+  ensure => running,
+  subscribe => File['/etc/mongodb.conf'],
 }
